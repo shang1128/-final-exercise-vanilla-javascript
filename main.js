9a86bc0fc3fb5ad5907fd9b994f161a8008d7633
shang1128 <centrii52@gmail.com>
2021-05-26 20:37:25 +0800

    continue js

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
+
+const darkmode = document.getElementById('darkmode');
+const darkicon = document.querySelector('.fa-moon');
 const addbtn = document.getElementById('add');
 const ul = document.getElementById('lists');
 const inputs = document.getElementById('inputs')
 const prefix = "shang-";
 const idGenPrefix = "id-generator";
 
-addbtn.addEventListener('click', function () {
-    // console.log('add');
+addbtn.addEventListener('click', AddItem);
+inputs.addEventListener('keydown', AddItemEnter);
+
+document.body.addEventListener('keydown', function(e){
+// console.log(e.keyCode);
+    if(e.keyCode===46){
+
+        const keys = Object.keys(localStorage);
+
+        const filter = keys.filter(key => key.includes(prefix));
+        for(let i = 0; i<filter.length; i++){
+
+            localStorage.removeItem(filter[i]);
+
+        }
+        updateUI();
+
+    }
+
+})
+
+
+function AddItem() {
     if (!inputs.value) {
         return;
     }
@@ -18,8 +42,16 @@ addbtn.addEventListener('click', function () {
     localStorageSetItem(inputs.value);
     updateUI();
     inputs.value = "";
+}
 
-});
+function AddItemEnter(e) {
+    if (e.keyCode === 13) {
+        AddItem();
+        e.preventDefault();
+    }
+}
+
+
 
 // ui
 function updateUI() {
@@ -30,15 +62,26 @@ function updateUI() {
         const value = localStorage.getItem(key);
         let li = document.createElement('li');
         let lisettings = document.createElement('div');
-        let icheck = document.createElement('i');
+
         let iopt = document.createElement('i');
         let iconoptions = document.createElement('div');
         let edit = document.createElement('a');
         let del = document.createElement('a');
 
+        let icheck = document.createElement('i');
+        let labelcheck = document.createElement('label')
+        let inputcheck = document.createElement('input')
+
+        
+
 
         lisettings.classList.add('lists-settings');
+
         icheck.classList.add('fa', 'fa-check', 'complete');
+        labelcheck.classList.add('checklabel');
+        inputcheck.classList.add('check');
+        inputcheck.setAttribute('type', 'checkbox');
+
         iopt.classList.add('fas', 'fa-ellipsis-v', 'option-icon');
         iconoptions.classList.add('options');
         edit.classList.add('edit', 'fas', 'fa-pen-square');
@@ -55,6 +98,10 @@ function updateUI() {
             }
             return;
         })
+        inputcheck.addEventListener('click', function(){
+            // console.log('ok')
+            li.classList.toggle('complete');
+        })
 
         edit.addEventListener('click', function () {
             const newText = prompt(`Update ${value}`);
@@ -66,11 +113,13 @@ function updateUI() {
             return;
         })
 
+        labelcheck.appendChild(inputcheck)
+        icheck.appendChild(labelcheck);
         iconoptions.appendChild(edit);
         iconoptions.appendChild(del);
         iopt.appendChild(iconoptions);
-        lisettings.appendChild(icheck);
         lisettings.appendChild(iopt);
+        lisettings.appendChild(icheck);
         li.appendChild(lisettings);
         ul.appendChild(li);
 
@@ -106,3 +155,9 @@ function isDuplicate(value) {
 }
 
 updateUI();
+
+//darkmode
+darkmode.addEventListener('click', function(){
+    document.body.classList.toggle('bodydarkmode');
+    darkicon.classList.toggle('dmode');
+})
